9f141f67d1d2e47b1fc71108567e6ed1b429bd08
smel <65329217+officialsmel@users.noreply.github.com>
2020-05-16 18:32:29 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    document.body.className += " loaded";
+}
+
+console.log("HEY! DONT JUST COPY THIS")
+
+console.log("ATLEAST CREDIT ME OR SOMETHING")
+
+// MADE BY SMEL github.com/officialsmel
